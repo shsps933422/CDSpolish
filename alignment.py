3462c2a29c78fa8994146ecf3fdb938cb9856dc6
shsps933422 <shsps933422@gmail.com>
2020-09-08 17:31:46 +0800

    Edit comment

--- alignment.py
+++ alignment.py
@@ -1,31 +1,27 @@
-from Bio import SeqIO
-import numpy as np
 import sys
 import math
+import numpy as np
+from Bio import SeqIO
 
 LONG_DELETION_LENGTH = 25
 DB_COVERAGE=10
 
 def aligment(filename, Genome_size,genome_id):
     """[summary]
-    
     Arguments:
         filename {paf file name} -- db alignment result
-        Genome_size {integer} -- TGS assembly only genome
-    
+        Genome_size {integer} -- TGS assembly only genome    
     Returns:
         arr1: genome each base [0-3]: ATCG, [4]: deletion
         total: genome each base total alignment result
         Ins: genome each base [0-3]: 1st Ins, 2nd Ins, 3rd Ins; [0-3]: ATCG
+	total_weight: genome each base alignment result of the total weight
     """
     arr1 = np.zeros((Genome_size,5), dtype=np.float)
     total = np.zeros(Genome_size, dtype=np.int)
-    #total2 = np.zeros(Genome_size, dtype=np.int)
     Ins = np.zeros((Genome_size, 4, 4), dtype=np.float)
-    
     total_weight = np.zeros(Genome_size, dtype=np.float)
-    
-    #total_gen = np.zeros((Genome_size,5), dtype=np.int)
+  
     line_count = 0  
     over_ins = []  
     db_dict={}
@@ -33,8 +29,7 @@ def aligment(filename, Genome_size,genome_id):
     with open(filename, 'r') as f:      
         for line in f:
             line_count += 1 #paf: the number of lines
-            line = line.split()      
-            #mapping quality == 60 , tp:A:P, 
+            line = line.split() 
             db_id=line[0]
             identity=float(line[13])
             query_cover=float(line[6])
@@ -54,10 +49,11 @@ def aligment(filename, Genome_size,genome_id):
             longdel_count = 0
             longdel_status = 0
             rank=0
-            for i in cigar: # ex. =ATC*c
+		
+            for i in cigar: 
                 db_dict.setdefault(start_pos,{})[db_id]=identity
                 
-                if i == 'A' or i == 'a':  # A:0, T:1, C:2, G:3
+		if i == 'A' or i == 'a':  # A:0, T:1, C:2, G:3
                     base = 0
                 elif i == 'T' or i == 't':
                     base = 1
@@ -65,23 +61,21 @@ def aligment(filename, Genome_size,genome_id):
                     base = 2
                 elif i == 'G' or i == 'g':
                     base = 3
-                    
-                
-                    
-                if i == '=': #match:1
+		
+                if i == '=': #match: 1
                     flag = 1
-                elif i == '*': #mismatch:2
+                elif i == '*': #mismatch: 2
                     flag = 2
                     mismatch = 0
-                elif i == '+': #insertion
+                elif i == '+': #insertion: 3
                     flag = 3
                     Ins_pos = 0
                     over_ins = []
-                elif i == '-': #deletion
+                elif i == '-': #deletion: 4
                     flag = 4
                     longdel_status = 0
                     
-                elif flag == 1:
+                elif flag == 1: #match
                     longdel_count = 0     
                     rank = list(db_dict[start_pos].keys()).index(db_id)
                     if rank <=2 :                       
@@ -92,7 +86,8 @@ def aligment(filename, Genome_size,genome_id):
                         total_weight[start_pos] += DB_COVERAGE/math.pow(2,3)              
                     total[start_pos] += 1
                     start_pos += 1 
-                elif flag == 2: 
+			
+                elif flag == 2: #mismatch
                     longdel_count = 0
                     if mismatch != 1:
                         mismatch += 1
@@ -107,39 +102,26 @@ def aligment(filename, Genome_size,genome_id):
                         total[start_pos] += 1 
                         start_pos += 1
                         
-                elif flag == 3:  #insertion
-                    #+AAAAA
-                    #-0123
-                    #01234
+                elif flag == 3: #insertion
                     longdel_count = 0
                     if Ins_pos < 4:
                         rank = list(db_dict[start_pos-1].keys()).index(db_id)
                         if rank <=2 : 
-                            Ins[start_pos-1][Ins_pos][base] += DB_COVERAGE/math.pow(2,rank)
-                            #total_weight[start_pos-1] += DB_COVERAGE/math.pow(2,rank) 
+                            Ins[start_pos-1][Ins_pos][base] += DB_COVERAGE/math.pow(2,rank)                           
                         else:
-                            Ins[start_pos-1][Ins_pos][base] += DB_COVERAGE/math.pow(2,3)
-                            #total_weight[start_pos-1] += DB_COVERAGE/math.pow(2,3) 
+                            Ins[start_pos-1][Ins_pos][base] += DB_COVERAGE/math.pow(2,3)                           
                         Ins_pos += 1
                         over_ins.append(base)
                     elif Ins_pos == 4:
-                        # for i in range(4):
-                        #     for j in over:
-                        #         Ins[start_pos-1][i][j] = 0
-                        
-                        # Ins_pos += 1
                         for x,y in zip(range(4), over_ins):
                             rank = list(db_dict[start_pos-1].keys()).index(db_id)
                             if rank <=2 : 
                                 Ins[start_pos-1][x][y] -= DB_COVERAGE/math.pow(2,rank) 
-                                #total_weight[start_pos-1] -= DB_COVERAGE/math.pow(2,rank) 
                             else:
                                 Ins[start_pos-1][x][y] -= DB_COVERAGE/math.pow(2,3)
-                                #total_weight[start_pos-1] -= DB_COVERAGE/math.pow(2,3) 
                         over_ins = []
-                            
-                            
-                elif flag == 4:    #deletion  
+                                                        
+                elif flag == 4: #deletion  
                        
                     if longdel_status == 0:
                         longdel_count += 1
@@ -169,20 +151,15 @@ def aligment(filename, Genome_size,genome_id):
         fp=open("identity.txt",'w')   
         for k,v in sorted(db_dict.items()):
 	          fp.write(str(k)+' '+str(v)+'\n')
-        #fp.write(db_dict)
         fp.close()
-        return arr1, total_weight, Ins#, cds
+        return arr1, total_weight, Ins
     
 def align(read, db_paf, truth_paf):
     record = SeqIO.read(read,"fasta")
     Genome_size = len(record)
     genome_id=record.id
-    #db_arr, db_base, db_Ins,db_cds = aligment(db_paf, Genome_size,gff,genome_id)
     db_arr, db_base, db_Ins = aligment(db_paf, Genome_size,genome_id)
-    
-    #np.savez('db.npz', db_arr, db_base, db_Ins, db_cds)
     np.savez('db.npz', db_arr, db_base, db_Ins)
-    
     return 'db.npz'
     
 align(sys.argv[1], sys.argv[2], sys.argv[3])
