aa02b556cd53a53e0c232956654d98eb1a35739f
shsps933422 <shsps933422@gmail.com>
2020-09-08 17:47:53 +0800

    Modify code

--- predict.py
+++ predict.py
@@ -1,13 +1,13 @@
-import pandas as pd
 import sys
-from libsvm.svmutil import *
-import preprocessing as p
-from sklearn.metrics import confusion_matrix,classification_report
-from sklearn import preprocessing
 import time
-from multiprocessing import Pool
-import numpy as np
 import joblib
+import numpy as np
+import pandas as pd
+import preprocessing as p
+
+from multiprocessing import Pool
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix,classification_report
 
 start = time.time()
 predict_alignment = sys.argv[1]
@@ -18,9 +18,7 @@ Y = X['label'].values
 position = X['position']
 X = p.double_ins_chaos(X)
 X = p.preprocessing(X)
-#X = pd.get_dummies(X, columns=['indel_3n'])
 X = pd.DataFrame(X.drop(['position','label'], axis=1))
-
 
 print(X.columns)
 print('training shape: ', X.shape)
@@ -47,20 +45,15 @@ ACC, MSE, SCC = evaluations(Y.tolist(), result)
 print(ACC)
 print(confusion_matrix(Y.tolist(),result))
 
-
 sub = X
 sub['position'] = position
 sub['label'] = Y
 sub['predict'] = result
-#sub.to_hdf('result.h5', key='sub', mode='w')
-# sub.to_csv('result.csv', index=False)
 sub.to_feather('result.feather')
-
 
 debug = sub[sub['predict'] != sub['label']]
 debug.to_csv('debug.csv', index=False)
 right = sub[sub['predict'] == sub['label']]
 right.to_csv('right.csv', index=False)
-#debug.to_hdf('debug.h5', key='debug', mode='w')
 end = time.time()
 print(end-start)
